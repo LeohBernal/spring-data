65c52c9b334ca31e0e5843a644646836e9c8da05
Leonardo Bernal <leonardo.bernal@unesp.br>
2021-01-04 22:05:34 -0300

    added pagination and ordernation to funcionario repository

--- src/main/java/br/com/springdata/projetospringdata/repository/FuncionarioRepository.java
+++ src/main/java/br/com/springdata/projetospringdata/repository/FuncionarioRepository.java
@@ -2,6 +2,7 @@ package br.com.springdata.projetospringdata.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.springdata.projetospringdata.orm.Funcionario;
@@ -10,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
     List<Funcionario> findByNome(String nome);
 
     @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
